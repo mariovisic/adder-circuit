cae04618d5ff7be64ff2d64a6fb3c426acdeb5f2
Mario Visic <mario@mariovisic.com>
2017-10-04 16:41:15 +1100

    Add two gates, the and as well as not gates

--- src/App.js
+++ src/App.js
@@ -9,6 +9,36 @@ const AppHeader = () => (
   </header>
 )
 
+const battery = true
+
+const transistor = (source, gate) => (
+  !!(source && gate)
+)
+
+const cTransistor = (source, gate) => (
+  !!(source && !gate)
+)
+
+const andGate = (a, b) => {
+  return transistor(transistor(battery, a), b)
+}
+
+const notGate = (a) => {
+  const output = cTransistor(battery, a)
+
+  transistor(output, a)
+
+  return output
+}
+
+console.log(`andGate(0, 0): ${andGate(0, 0)}`)
+console.log(`andGate(0, 1): ${andGate(0, 1)}`)
+console.log(`andGate(1, 0): ${andGate(1, 0)}`)
+console.log(`andGate(1, 1): ${andGate(1, 1)}`)
+
+console.log(`notGate(0): ${notGate(0)}`)
+console.log(`notGate(1): ${notGate(1)}`)
+
 class App extends Component {
   render() {
     return (
